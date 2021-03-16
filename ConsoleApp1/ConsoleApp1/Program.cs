f63d1930ef7b64beb255934d9b9b45dce57c8cb6
Hoffe <djkagemand1@gmail.com>
2021-03-16 11:03:33 +0100

    Nem

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,11 +2,80 @@
 
 namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        }
+
+        public static List<int> bubblesort(List<int> numbers)
+        {
+            int temp;
+
+            for (int j = 0; j <= numbers.Count - 2; j++)
+            {
+                for (int i = 0; i <= numbers.Count - 2; i++)
+                {
+                    if (numbers[i] > numbers[i + 1])
+                    {
+                        temp = numbers[i + 1];
+                        numbers[i + 1] = numbers[i];
+                        numbers[i] = temp;
+                    }
+                }
+            }
+
+            return numbers;
+        }
+
+        public static List<int> Insertionsort(List<int> numbers)
+        {
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (numbers[j - 1] > numbers[j])
+                    {
+                        int temp = numbers[j - 1];
+                        numbers[j - 1] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+            return numbers;
+        }
+
+        public static List<int> Quick(List<int> n)
+        {
+            if (n.Count <= 1)
+            {
+                return n;
+            }
+
+            int pivot = n[0];
+
+            List<int> b = new List<int>();
+            List<int> a = new List<int>();
+
+            for (int i = 1; i < n.Count; i++)
+            {
+                if (n[i] < pivot)
+                {
+                    b.Add(n[i]);
+                }
+                else
+                {
+                    a.Add(n[i]);
+                }
+            }
+
+            List<int> r = new List<int>();
+
+            r.AddRange(Quick(b));
+            r.Add(pivot);
+            r.AddRange(Quick(a));
+
+            return r;
         }
     }
 }
---
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void BubbleSort()
+        {
+        }
+
+        public void QuickSort()
+        {
+        }
+
+        public void Insertionsort()
+        {
+        }
+    }
+}
